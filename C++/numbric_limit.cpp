668905e2ee67ef507672bbb935af33e6bc3347d1
shantu03 <hulwanshantanu@gmail.com>
2023-04-14 12:22:00 +0530

    on pop os

--- C++/numbric_limit.cpp
+++ C++/numbric_limit.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main()
 {
-    cout<<numeric_limits<int>::min()<<" to "<<numeric_limits<int>::max()<<endl;
-    
-    cout<<"in digit "<<numeric_limits<float>::digits<<endl;
+    cout<<"for integer"<<numeric_limits<int>::min()<<" to "<<numeric_limits<int>::max()<<endl;
+	
+	cout<<"for double"<<numeric_limits<double>::min()<<" to "<<numeric_limits<double>::max()<<endl;
+	cout<<"for float"<<numeric_limits<float>::min()<<" to "<<numeric_limits<float>::max()<<endl;
+
+ 	   cout<<"in digit float "<<numeric_limits<float>::digits<<endl;
     cout<<"in digit double"<<numeric_limits<double>::digits<<endl;
     
    cout<<"in digit int"<<numeric_limits<int>::digits<<endl;
