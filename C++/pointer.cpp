b6a25b9d1fa379850340c81a455fd022937a9c2e
shantu03 <hulwanshantanu@gmail.com>
2023-04-17 19:03:36 +0530

    on Pop os

--- C++/pointer.cpp
+++ C++/pointer.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
+#include<ios>
 int main()
 {
     int **hel,*p;
-    int k;
+    int k,i{8};
     k=9;
     p=&k;
     hel=&p;
-  //  std::cout<<*p<<std::endl;
-    std::cout<<&hel<<std::endl<<hel<<std::endl<<*hel<<std::endl<<**hel;
+//    std::cout<<&p<<std::endl<<p<<std::endl<<*p<<std::endl;
+//std::cout<<"while &k =p  " <<p<<std::endl;
+    p=&i;
+//std::cout<<"while &i =p  "<<p<<std::endl;
+//    std::cout<<&hel<<std::endl<<hel<<std::endl<<*hel<<std::endl<<**hel;
+
+//declaring pointer to char
+char *q,char_var{'S'};
+q=&char_var;
+std::cout<<q<<std::endl;
+
+
+// initialize with string literal
+char *msg {"hello world !"};
+std::cout<<msg<<std::endl;
+std::cout<<*msg<<std::endl;
+
 }
