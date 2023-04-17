299497f1a662a9df322687e0901dc2364bc8c53f
shantu03 <hulwanshantanu@gmail.com>
2023-04-17 08:06:19 +0530

    on pop os

--- C++/werid_integral.cpp
+++ C++/werid_integral.cpp
@@ -5,9 +5,18 @@ int main()
 {
 short int k;
 short int t;
+
+char s {3};
+char l  {7};
 cin>>k>>t;
+cout<<"size of variable (short int)"<<endl;
 cout<<sizeof(k)<<"\t"<<sizeof(t)<<endl;
 auto p=k+t;
-cout<<p<<endl;
-cout<<sizeof(p)<<endl;
+;
+
+cout<<"result " <<p<<endl;
+cout<<"size of p(int additon ) is " <<sizeof(p)<<endl;
+cout<<endl;
+auto j= s+l;
+cout<<"result of char "<<j<<"  size of j (char additon ) is   "<<sizeof(j)<<endl;
 }
