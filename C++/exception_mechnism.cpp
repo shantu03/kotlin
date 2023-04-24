81de8d85a0e4d96de25a7b2fcfc6025f7630ecca
shantu03 <hulwanshantanu@gmail.com>
2023-04-24 11:20:41 +0530

    on pop os

--- C++/exception_mechnism.cpp
+++ C++/exception_mechnism.cpp
@@ -11,5 +11,18 @@ int main()
     }
     std::cout<<"this is good"<<std::endl;
 */
+
+//--> Try & Catch operator this operator help us to understand certain code fail or not 
+
+ try {
+int * data = new int [1000000000];   // in Try Bracket statememt will tested if error occured then--
+}
+catch(std::exception& ex)	//then catch funciton going to catch the error with exception and put in & ex which is accessed downside
+{
+std::cout<<" Error occured here" <<ex.what()<<std::endl;   	//ex.what() explain what  error occured in statement 
+}
+// if no error occured then compiler going to ignore catch  fuction
+
+std::cout<<"End Of the Program "<<std::endl;
     return 0;
 }
