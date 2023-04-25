6bb141a9af7dd7649ddbffeb4f1cac62b62f6918
shantu03 <hulwanshantanu@gmail.com>
2023-04-25 08:54:52 +0530

    on pop os t

--- C++/array.cpp
+++ C++/array.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
-
+#include<unistd.h>
 #include<time.h>
 using namespace std;
 int main()
 {
+/*
     int a[20]={1,2,3,4,5};
     int k;
     cout<<"enter elements of array  ";
@@ -18,4 +19,52 @@ int main()
     char x='J';
     cout<<(int)x;
     return 0;
+*/
+
+//11::56::42  --> Dynamic array allocation 
+size_t size{10};
+double *po {new double [size]};	//all value initilized to 0
+
+int *poo {new(nothrow) int [size+1000000]{1,2,3,4,5}};
+
+if(poo)
+{
+
+for(size_t i{0};i<size;i++)
+{
+
+cout<<"\t"<<*(poo+i)<<endl;
+usleep(150000);		//--> usleep is same as sleep but it measure in microseconds 
+}
+}
+else
+cout<<"array allocation failed"<<endl;
+
+delete[] po;		//----> to delete po array 
+delete[] poo;			//same as releasing memnory of array
+ po= nullptr;
+poo =nullptr;		//Just becoause we are good C++ programmer
+
+
+/* 
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+for(--:--) used for check size or print sizeth time statement checkot follows code for your information 
+cout<<"+++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
+int hu[size+2];
+int k=1;
+
+
+for( int temp : hu)
+{
+cout<<k<<"\t";k++;
+cout<<temp<<endl;
+}
+
+
+*/
+
+
+
+
 }
