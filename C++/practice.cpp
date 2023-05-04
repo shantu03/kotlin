ffd8672f561d1447ee98da70ffdd84ef88812195
shantu03 <hulwanshantanu@gmail.com>
2023-05-04 06:28:29 +0530

    on pop os

---
+++ C++/practice.cpp
@@ -0,0 +1,48 @@
+/*#include <stdio.h>
+#include<iostream>
+void update(int *a,int *b) {
+int k=*a;
+    *a+=*b;
+    *b-=k;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d\n", a, b);
+
+    return 0;
+}
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;
+int A,K;
+cin>>A>>K;
+int arr[A];
+for(size_t i {0};i<A;i++)
+{
+cin>>arr[i];
+}
+int t=A-(K%A);
+
+for(size_t i{0};i<t-1;i++)
+{
+cout<<arr[t+i]<<" ";
+}
+for(size_t i{0};i<=A-t;i++)
+{
+cout<<arr[i]<<" ";
+}
+
+cout<<endl;
+return 0;
+}
+
