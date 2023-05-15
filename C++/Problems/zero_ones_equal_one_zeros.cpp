e9560347e5d0748c85ca8111f4790b5432be181f
shantu03 <hulwanshantanu@gmail.com>
2023-05-15 19:08:51 +0530

    on pop os

---
+++ C++/Problems/zero_ones_equal_one_zeros.cpp
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/ZOOZ
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int cases;
+cin>>cases;
+while(cases)
+{
+int n;
+cin>>n;
+int arr[n];
+	if(n%2)
+	{
+	cout<<"even"<<endl;
+	}
+else
+cout<<"odd"<<endl;
+cases--;
+}
+return 0;
+}
