dc9a33daf6df0d5fce21209987e7b7ef993038c5
Shantanu Hulwan <117788434+shantu03@users.noreply.github.com>
2023-05-25 14:59:04 +0530

    Delete C++ directory

--- C++/boolapha.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main()
-{
-    std::cout<<std::boolalpha;   //used this boolalpha to print true or flase instead of 1 or 0
-    int a {7};
-    int k {8};
-    std::cout<<(a!=k);
-}
--- C++/he.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    float a=12.0*35.0/36.0;
-    cout<<float(a)<<endl;
-}
