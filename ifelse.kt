c21d8dc580698565f32d49c9f10cfb61dfbe70a9
shantu03 <hulwanshantanu@gmail.com>
2023-07-12 13:09:58 +0530

    creating kotlin folder under kotlin branch

--- ifelse.kt
+++
@@ -1,9 +0,0 @@
-fun main()
-{
-    var name =readLine()
-    println("Enter your number")
-    //follow input line is extra
-var y:Int=Integer.valueOf(readLine())
-    val result=if(y%2==0) "Even " else "odd "
-println("name is $name and result is $result")
-}
